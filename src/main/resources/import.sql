1956332821c60e64ac15fb3a1929554a6534ff0c
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-04-18 14:31:50 +0800

    ...

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,24 +1,7 @@
-CREATE TABLE authorities
-(
-  id        bigint auto_increment  not null,
-  username  varchar_ignorecase(50) not null,
-  authority varchar_ignorecase(50) not null,
-  constraint fk_authorities_users foreign key (username) references users (username)
-);
-CREATE TABLE users
-(
-  id       bigint auto_increment  not null,
-  username varchar_ignorecase(50) not null,
-  password varchar(50)            not null,
-  enabled  boolean                not null
-);
-INSERT INTO users (username, password, enabled)
-VALUES ('root', 'root', true);
-INSERT INTO users (username, password, enabled)
-VALUES ('user', 'user', true);
-INSERT INTO authorities (username, authority)
-VALUES ('root', 'ROLE_user');
-INSERT INTO authorities (username, authority)
-VALUES ('root', 'ROLE_admin');
-INSERT INTO authorities (username, authority)
-VALUES ('user', 'ROLE_user');
+CREATE TABLE users (id bigint auto_increment not null, username varchar_ignorecase(50) not null, password varchar_ignorecase(50) not null, enabled boolean not null);
+CREATE TABLE authorities (id bigint auto_increment not null, username varchar_ignorecase(50) not null, authority varchar_ignorecase(50) not null, constraint fk_authorities_users foreign key (username) references users (username));
+INSERT INTO users (username, password, enabled) VALUES ('root', '{noop}root', true);
+INSERT INTO users (username, password, enabled) VALUES ('user', '{noop}user', true);
+INSERT INTO authorities (username, authority) VALUES ('root', 'ROLE_user');
+INSERT INTO authorities (username, authority) VALUES ('root', 'ROLE_admin');
+INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_user');
--- src/main/scala/com/atzu68/trial/springbootscala/configuration/WebSecurityConfig.scala
+++ src/main/scala/com/atzu68/trial/springbootscala/configuration/WebSecurityConfig.scala
@@ -2,11 +2,15 @@ package com.atzu68.trial.springbootscala.configuration
 
 import javax.sql.DataSource
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.{EnableWebSecurity, WebSecurityConfigurerAdapter}
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -34,10 +38,19 @@ class WebSecurityConfig(@Autowired val dataSource: DataSource)
     http.httpBasic()
   }
 
+  override def configure(auth: AuthenticationManagerBuilder): Unit =
+    auth.userDetailsService(userDetailsService()).
+      passwordEncoder(passwordEncoder())
+
+  @Bean
   override def userDetailsService(): UserDetailsService = {
 
     val manager = new JdbcDaoImpl
     manager.setDataSource(dataSource)
     manager
   }
+
+  @Bean
+  def passwordEncoder(): PasswordEncoder =
+    PasswordEncoderFactories.createDelegatingPasswordEncoder()
 }
